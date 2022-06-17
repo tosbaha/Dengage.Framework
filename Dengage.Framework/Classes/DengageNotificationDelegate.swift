9568084f3e828a82fd2cdfa15c8558768fdb878d
Mustafa <971530+tosbaha@users.noreply.github.com>
2022-06-17 22:55:17 +0300

    Remove URL open

--- Dengage.Framework/Classes/DengageNotificationDelegate.swift
+++ Dengage.Framework/Classes/DengageNotificationDelegate.swift
@@ -102,7 +102,7 @@ class DengageNotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
     final func openDeeplink(link: String?) {
         guard let urlString = link, let url = URL(string: urlString) else { return }
         os_log("TARGET_URL is %s", log: .default, type: .debug, link!)
-        UIApplication.safeShared?.open(url, options: [:], completionHandler: nil)
+//        UIApplication.safeShared?.open(url, options: [:], completionHandler: nil)
     }
 
     final func checkTargetUrl(content: UNNotificationContent) {
